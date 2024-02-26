a04e3f141e563b4341e30c543697ebf27d844d90
kuldeep singh <kuldeepsb.bsc23@rvu.edu.in>
2024-02-26 23:53:29 +0530

    set01

---
+++ set01/problem08.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main()
+{
+    int n = input_array_size();
+    int a[n];
+    input_array(n,a);
+    int sum = sum_n_array(n,a);
+    output(sum);
+    return 0;
+}
+int input_array_size()
+{
+    int n;
+    printf("input array size:");
+    for(int i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+
+    }
+}
